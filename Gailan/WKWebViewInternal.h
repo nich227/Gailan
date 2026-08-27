fa455ccae7160035dd4a94fa7e663820341132c1
nich227 <nich227@users.noreply.github.com>
2026-08-27 12:30:25 -0700

    Declare _pageForTesting as a category, not a second WKWebView
    The header declared @interface WKWebView : NSView from scratch, which clashes
    with WebKit's own definition as soon as anything in the same build imports the
    framework. A category says the same thing without redefining the class.

--- Gailan/WKWebViewInternal.h
+++ Gailan/WKWebViewInternal.h
@@ -1,14 +1,21 @@
 /*
- * https://github.com/WebKit/webkit/blob/master/Source/WebKit2/UIProcess/API/Cocoa/WKWebViewInternal.h
+ * Reaches _pageForTesting, which is how the debug console gets at the page
+ * behind a web view. Declared as a category on the real class: redeclaring
+ * WKWebView outright, as this header used to, conflicts with WebKit's own
+ * definition once anything imports the framework.
+ *
+ * See https://github.com/WebKit/webkit/blob/master/Source/WebKit2/UIProcess/API/Cocoa/WKWebViewInternal.h
  * Copyright (C) 2010 Apple Inc. All rights reserved.
  */
 
 #ifndef WKWebViewInternal_h
 #define WKWebViewInternal_h
 
+#import <WebKit/WebKit.h>
 #include "WKBase.h"
 
-@interface WKWebView : NSView
+@interface WKWebView (GailanInternal)
 - (WKPageRef)_pageForTesting;
 @end
+
 #endif
